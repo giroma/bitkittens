d0a9890942703215a3f65bcc086b656f56a61f2f
Argir Argirov <dj.giro@gmail.com>
2017-11-15 17:04:09 -0500

    further refactoring with jquery

--- cats.js
+++ cats.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+$().ready( function() {
   $('.summon-cats').click( function () {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET'
     }).done(function (response) {
       for (var i = 0; i < 3; i++) {
+        $(`#cat${i+1}`).html('') //remove last picture before adding new one
         $(`#cat${i+1}`).append(`<img src=${response.cats[i].photo} />`)
       }
     })
