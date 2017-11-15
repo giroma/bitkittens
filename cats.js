a208bb4c83166f9d9658b589165c1b28e46aada8
Argir Argirov <dj.giro@gmail.com>
2017-11-15 16:41:41 -0500

    much shorter solution with JQuery

--- cats.js
+++ cats.js
@@ -1,26 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var catButton = document.querySelector('.summon-cats')
-
-
-  catButton.addEventListener('click', function () {
+  $('.summon-cats').click( function () {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET'
     }).done(function (response) {
-      var cats = response['cats']
       for (var i = 0; i < 3; i++) {
-
-        var image = document.createElement('img')
-        var parent = document.querySelector('#cat'+(i+1))
-        parent.innerHTML = ''
-        console.log(parent);
-        image.src = cats[i]['photo']
-        console.log(image);
-        parent.append(image)
+        $(`#cat${i+1}`).append(`<img src=${response.cats[i].photo} />`)
       }
-
     })
   })
-
 });
