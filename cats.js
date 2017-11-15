c366c4594a8fb0e808055c4a2f16447522d980df
Argir Argirov <dj.giro@gmail.com>
2017-11-15 16:05:00 -0500

    even listener added to the kitties button

---
+++ cats.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var catButton = document.querySelector('.summon-cats')
+
+
+  catButton.addEventListener('click', function () {
+    console.log('hello');
+  })
+
+
+});
