75b551faf6dd237e0f9166d3a2ee6fb7fbf25818
Argir Argirov <dj.giro@gmail.com>
2017-11-15 16:25:33 -0500

    random cats generated from api

--- cats.js
+++ cats.js
@@ -4,8 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   catButton.addEventListener('click', function () {
-    console.log('hello');
-  })
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET'
+    }).done(function (response) {
+      var cats = response['cats']
+      for (var i = 0; i < 3; i++) {
 
+        var image = document.createElement('img')
+        var parent = document.querySelector('#cat'+(i+1))
+        parent.innerHTML = ''
+        console.log(parent);
+        image.src = cats[i]['photo']
+        console.log(image);
+        parent.append(image)
+      }
+
+    })
+  })
 
 });
